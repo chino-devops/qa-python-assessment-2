fc58403c996602f2865b200de8f0afda65c306f1
chino-devops <danielnwankwo95@gmail.com>
2021-06-18 11:03:54 +0000

    answers

---
+++ answers/#2.py
@@ -0,0 +1,9 @@
+def two(number):
+    if number > 1:
+        for i in range(2, number):
+            if num % i == 0:
+                return True
+            else:
+                return False
+
+    
